9f48a763a9751983da2adae89495dbe2cbb73b25
Ezewuzie Okafor <eokafor@seamfix.com>
2016-09-21 13:38:24 +0100

    meta model classes generated

---
+++ ims-jpa/src/com/kyc/incentives/ImsRole_.java
@@ -0,0 +1,12 @@
+package com.kyc.incentives;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="Dali", date="2016-09-21T13:11:17.686+0100")
+@StaticMetamodel(ImsRole.class)
+public class ImsRole_ extends BaseModel_ {
+	public static volatile SingularAttribute<ImsRole, String> name;
+	public static volatile SingularAttribute<ImsRole, String> code;
+}
