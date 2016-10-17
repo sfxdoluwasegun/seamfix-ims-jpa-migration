0a5a0b421013a08486eae0f63c06eb4a2e81e906
Ezewuzie Okafor <eokafor@seamfix.com>
2016-10-17 10:23:52 +0100

    added the column for referencing the corresponding role in kyc #IMS-55

--- ims-jpa/src/com/kyc/incentives/ImsRole_.java
+++ ims-jpa/src/com/kyc/incentives/ImsRole_.java
@@ -4,9 +4,10 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2016-09-21T13:11:17.686+0100")
+@Generated(value="Dali", date="2016-10-17T10:23:09.788+0100")
 @StaticMetamodel(ImsRole.class)
 public class ImsRole_ extends BaseModel_ {
 	public static volatile SingularAttribute<ImsRole, String> name;
 	public static volatile SingularAttribute<ImsRole, String> code;
+	public static volatile SingularAttribute<ImsRole, Long> referencedId;
 }
