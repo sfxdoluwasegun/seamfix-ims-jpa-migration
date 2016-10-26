7f58f2bdbed4f1a11333b75f8ecc5bbd9a853f86
Ezewuzie Okafor <eokafor@seamfix.com>
2016-10-26 12:35:55 +0100

    changed the group id and artifact id

--- ims-jpa/src/com/kyc/incentives/AppUser_.java
+++ ims-jpa/src/com/kyc/incentives/AppUser_.java
@@ -5,12 +5,12 @@ import javax.persistence.metamodel.SetAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2016-10-25T09:51:57.392+0100")
+@Generated(value="Dali", date="2016-10-25T14:45:11.165+0100")
 @StaticMetamodel(AppUser.class)
 public class AppUser_ extends BaseModel_ {
 	public static volatile SingularAttribute<AppUser, Long> orbitaId;
 	public static volatile SingularAttribute<AppUser, String> email;
 	public static volatile SingularAttribute<AppUser, String> name;
-	public static volatile SingularAttribute<AppUser, Long> referencedId;
 	public static volatile SetAttribute<AppUser, ImsRole> roles;
+	public static volatile SingularAttribute<AppUser, Long> referencedId;
 }
