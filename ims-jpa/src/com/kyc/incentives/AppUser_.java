5f190a6e98107313be165dc99dcc499a1d1e98d3
Ezewuzie Okafor <eokafor@seamfix.com>
2016-10-27 10:52:40 +0100

    made the name non-unique #IMS-65

--- ims-jpa/src/com/kyc/incentives/AppUser_.java
+++ ims-jpa/src/com/kyc/incentives/AppUser_.java
@@ -5,12 +5,12 @@ import javax.persistence.metamodel.SetAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2016-10-25T14:45:11.165+0100")
+@Generated(value="Dali", date="2016-10-27T10:51:06.532+0100")
 @StaticMetamodel(AppUser.class)
 public class AppUser_ extends BaseModel_ {
 	public static volatile SingularAttribute<AppUser, Long> orbitaId;
 	public static volatile SingularAttribute<AppUser, String> email;
 	public static volatile SingularAttribute<AppUser, String> name;
-	public static volatile SetAttribute<AppUser, ImsRole> roles;
 	public static volatile SingularAttribute<AppUser, Long> referencedId;
+	public static volatile SetAttribute<AppUser, ImsRole> roles;
 }
